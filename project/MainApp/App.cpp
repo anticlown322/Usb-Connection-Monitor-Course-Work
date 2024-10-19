0d9eaa8e9ca5fabbc451d158fc02d97232d56ef3
anticlown322 <karasandrey2005@gmail.com>
2024-10-19 22:28:45 +0300

    feat: implement transparency with direct2d

--- project/MainApp/App.cpp
+++ project/MainApp/App.cpp
@@ -1,4 +1,5 @@
 #include "general.h"
+#include "basewin.h"
 #include "App.h"
 
 HRESULT MainWindow::CreateGraphicsResources()
@@ -7,24 +8,77 @@ HRESULT MainWindow::CreateGraphicsResources()
 
     if (pRenderTarget == NULL)
     {
+        /* window */
         RECT clientRect;
         GetClientRect(m_hwnd, &clientRect);
 
-        D2D1_SIZE_U size = D2D1::SizeU(clientRect.right, clientRect.bottom);
-
+        D2D1_SIZE_U size = D2D1::SizeU(
+            clientRect.right - clientRect.left,
+            clientRect.bottom - clientRect.top
+        );
+        
+        /* render */
         hResult = pFactory->CreateHwndRenderTarget(
-            D2D1::RenderTargetProperties(),
+            D2D1::RenderTargetProperties(
+                D2D1_RENDER_TARGET_TYPE_DEFAULT, 
+                D2D1::PixelFormat(DXGI_FORMAT_UNKNOWN, D2D1_ALPHA_MODE_PREMULTIPLIED)),
             D2D1::HwndRenderTargetProperties(m_hwnd, size),
             &pRenderTarget
         );
 
         if (SUCCEEDED(hResult))
         {
-            //creation
+            /* background  brush */
+            ID2D1GradientStopCollection* pGradientStops = NULL;
+            D2D1_GRADIENT_STOP gradientStops[2];
+            gradientStops[0].color = D2D1::ColorF(D2D1::ColorF::Blue, 1);
+            gradientStops[0].position = 0.0f;
+            gradientStops[1].color = D2D1::ColorF(D2D1::ColorF::Violet, 1);
+            gradientStops[1].position = 1.0f;
+
+            hResult = pRenderTarget->CreateGradientStopCollection(
+                gradientStops,
+                2,
+                D2D1_GAMMA_2_2,
+                D2D1_EXTEND_MODE_CLAMP,
+                &pGradientStops
+            );
 
             if (SUCCEEDED(hResult))
             {
-                //first usage
+                hResult = pRenderTarget->CreateLinearGradientBrush(
+                    D2D1::LinearGradientBrushProperties(
+                        D2D1::Point2F(clientRect.left, clientRect.top),
+                        D2D1::Point2F(clientRect.right, clientRect.bottom)),
+                    pGradientStops,
+                    &pBackgroudnGradientBrush
+                );
+            }
+
+            /* border brush */
+            pGradientStops = NULL;
+            gradientStops[0].color = D2D1::ColorF(D2D1::ColorF::Violet, 1);
+            gradientStops[0].position = 0.0f;
+            gradientStops[1].color = D2D1::ColorF(D2D1::ColorF::Blue, 1);
+            gradientStops[1].position = 1.0f;
+
+            hResult = pRenderTarget->CreateGradientStopCollection(
+                gradientStops,
+                2,
+                D2D1_GAMMA_2_2,
+                D2D1_EXTEND_MODE_CLAMP,
+                &pGradientStops
+            );
+
+            if (SUCCEEDED(hResult))
+            {
+                hResult = pRenderTarget->CreateLinearGradientBrush(
+                    D2D1::LinearGradientBrushProperties(
+                        D2D1::Point2F(clientRect.left, clientRect.top),
+                        D2D1::Point2F(clientRect.right, clientRect.bottom)),
+                    pGradientStops,
+                    &pWindowBorderBrush
+                );
             }
         }
     }
@@ -34,6 +88,8 @@ HRESULT MainWindow::CreateGraphicsResources()
 
 void MainWindow::DiscardGraphicsResources()
 {
+    SafeRelease(&pBackgroudnGradientBrush);
+    SafeRelease(&pWindowBorderBrush);
     SafeRelease(&pRenderTarget);
 }
 
@@ -43,18 +99,31 @@ void MainWindow::OnPaint()
 
     if (SUCCEEDED(hResult))
     {
-        PAINTSTRUCT paintStruct;
-        BeginPaint(m_hwnd, &paintStruct);
+        /* prepare for drawing */
+        //PAINTSTRUCT paintStruct;
+        //BeginPaint(m_hwnd, &paintStruct);
         pRenderTarget->BeginDraw();
+        pRenderTarget->Clear();
 
-        pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::SkyBlue));
+        /* get sizes */
+        D2D1_SIZE_F clientSizes = pRenderTarget->GetSize();
+
+        /* draw window backgorund and border */
+        D2D1_ROUNDED_RECT roundedWindowRect = D2D1::RoundedRect(
+            D2D1::RectF(0, 0, clientSizes.width, clientSizes.height),
+            30.f,
+            30.f
+        );
+
+        pRenderTarget->FillRoundedRectangle(&roundedWindowRect, pBackgroudnGradientBrush);
+        pRenderTarget->DrawRoundedRectangle(&roundedWindowRect, pWindowBorderBrush, 2.0f, NULL);
 
         hResult = pRenderTarget->EndDraw();
         if (FAILED(hResult) || hResult == D2DERR_RECREATE_TARGET)
         {
             DiscardGraphicsResources();
         }
-        EndPaint(m_hwnd, &paintStruct);
+        //EndPaint(m_hwnd, &paintStruct);
     }
 }
 
@@ -70,27 +139,6 @@ void MainWindow::Resize()
         pRenderTarget->Resize(size);
         InvalidateRect(m_hwnd, NULL, FALSE);
     }
-}
-
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
-{
-    MainWindow win;
-
-    if (!win.Create(L"Circle", WS_OVERLAPPEDWINDOW))
-    {
-        return 0;
-    }
-
-    ShowWindow(win.Window(), nCmdShow);
-
-    MSG msg = { };
-    while (GetMessage(&msg, NULL, 0, 0))
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-
-    return 0;
 }
 
 LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -121,4 +169,42 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
     }
 
     return DefWindowProc(m_hwnd, uMsg, wParam, lParam);
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
+{
+    MainWindow win;
+
+    BOOL creationResult = win.Create(
+        L"USB Monitor",
+        WS_POPUP | WS_EX_TOPMOST,
+        WS_EX_LAYERED, //for transparent d2d1 clear
+        (SCREEN_WIDTH - MainWindow::startWndWidth) / 2,
+        (SCREEN_HEIGHT - MainWindow::startWndHeight) / 2,
+        MainWindow::startWndWidth,
+        MainWindow::startWndHeight
+    );
+
+    if (!creationResult)
+        return EXIT_FAILURE;
+
+    ShowWindow(win.Window(), nCmdShow);
+
+    /* message loop */
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return static_cast<int>(msg.message);
+}
+
+MainWindow::MainWindow()
+{
+    pFactory = nullptr;
+    pRenderTarget = nullptr;
+    pBackgroudnGradientBrush = nullptr;
+    pWindowBorderBrush = nullptr;
 }
