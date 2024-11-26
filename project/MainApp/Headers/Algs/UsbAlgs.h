05d24697171a277d7fe0aa0fb8f4308d7f24f508
anticlown322 <karasandrey2005@gmail.com>
2024-11-26 21:08:02 +0300

    feat: more info gathering

--- project/MainApp/Headers/Algs/UsbAlgs.h
+++ project/MainApp/Headers/Algs/UsbAlgs.h
@@ -55,19 +55,34 @@ struct USBDeviceInfo {
     std::wstring parentIDPrefix;
 };
 
+/* common */
 std::wstring GetRegistryStringValue(HKEY hKey, const std::wstring& valueName);
+DWORD GetRegistryDWORDValue(HKEY hKey, const std::wstring& valueName);
+std::vector<USBDeviceInfo> ListUSBDevices(const std::wstring& registryPath);
+std::vector<USBDeviceInfo> GetAllUsbDevices();
 
+/* basic info */
 std::wstring GetDeviceDescription(HKEY hKey);
 std::wstring GetDeviceManufactoring(HKEY hKey);
 
+/* status */
 bool IsDeviceConnected(HKEY hKey);
 bool IsDeviceDisabled(HKEY hKey);
 bool IsDeviceSafeToUnplug(HKEY hKey);
-bool IsUsbHub(HKEY hKey); 
+
+/* driver info */
+std::wstring GetDeviceDriverFilename(HKEY hKey);
+std::wstring GetDeviceDriverVersion(HKEY hKey);
+
+/* capabilities */
 std::wstring GetDeviceCapabilities(HKEY hKey);
 
-std::vector<USBDeviceInfo> ListUSBDevices(const std::wstring& registryPath);
-std::vector<USBDeviceInfo> GetAllUsbDevices();
+/* manufactoring info */
+std::wstring GetDeviceSerialNumber(HKEY hKey);
+std::wstring ExtractUsbField(const std::wstring& compatibleIDs, const std::wregex& pattern);
+std::wstring GetDeviceUsbClass(HKEY hKey);
+std::wstring GetDeviceUsbSubclass(HKEY hKey);
+std::wstring GetDeviceUsbProtocol(HKEY hKey);
 
 #endif // !USB_ALGS
 
